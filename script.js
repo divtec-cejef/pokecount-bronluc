0c5cf71f5d1b9d89ae5614029034c92bfcd2518a
Lucas Bron <lucas.bron@divtec.ch>
2025-08-19 15:57:17 +0200

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+/**
+ * Fichier JavaScript pour l'application PokeCount.
+ * @author Lucas Bron <lucas.bron@divtec.ch>
+ * @version 0.1 (Version actuelle)
+ * @since 2025-08-19 (Date de création)
+ */
+
+"use strict"; // Interprètation stricte du code
+console.log("Hello World"); // Affiche "Hello World" dans la console
