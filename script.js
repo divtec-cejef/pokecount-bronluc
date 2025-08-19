a83b7889782074797cc74df50dc577ff85ace719
Lucas Bron <lucas.bron@divtec.ch>
2025-08-19 16:13:29 +0200

    update

--- script.js
+++ script.js
@@ -15,5 +15,21 @@ const compteurEl = document.getElementById("compteur-el");
 
 function capturer() {
     compteur += 1; // Incrémenter le compteur de 1
-    compteurEl.textContent = compteur; // Actualiser le texte de l'élément <h2>
+    compteurEl.textContent = compteur; // Mettre à jour le texte de l'élément <h2>
+    if (compteur < 5) {
+        compteurEl.style.color = "green"; // Couleur verte pour moins de 5 captures
+    } else if (compteur < 10) {
+        compteurEl.style.color = "yellow"; // Couleur jaune pour 5 à 9 captures
+    } else {
+        compteurEl.style.color = "red"; // Couleur rouge pour 10 captures ou plus
+    }
+}
+
+const sauvegardeEl = document.getElementById("sauvegarde-el");
+
+function sauvegarder() {
+    let compteurStr = "<li>" + compteur + " Pokémons</li>";
+    sauvegardeEl.innerHTML += compteurStr; // Ajouter la valeur actuelle du compteur
+    compteur = 0;
+    compteurEl.textContent = compteur;
 }
